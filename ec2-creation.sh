647184d222474da6d6e87941026c101a49a12c04
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 15:00:02 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -8,9 +8,10 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" \
                                         --output text | grep roboshop.internal | awk '{print $1}' | awk -F / '{print $3}')
-PRIVATE_IP=(aws ec2 run-instances --image-id="${AMI_ID}" \
+PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
                       --instance-type=t2.micro \
                       --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
                       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                      --security-group-ids ${SGID} | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+                      --security-group-ids ${SGID} \
+                      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
