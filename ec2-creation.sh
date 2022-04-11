6fd289e36af59fa09b33214baab19ac7b84a9e27
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 15:42:48 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -7,13 +7,14 @@ COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 
+PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
-PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
-                      --instance-type=t2.micro \
-                      --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-                      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                      --security-group-ids ${SGID} \
-                      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+#PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
+#                      --instance-type=t2.micro \
+#                      --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+#                      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+#                      --security-group-ids ${SGID} \
+#                      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
                       
 if [ -z "${PRIVATE_IP}" ]; then
     SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
