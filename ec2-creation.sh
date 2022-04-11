5c512615b3d5fae4ea24ec5f94e2a3197892b31c
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 14:57:34 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -12,5 +12,5 @@ PRIVATE_IP=(aws ec2 run-instances --image-id="${AMI_ID}" \
                       --instance-type=t2.micro \
                       --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
                       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                      --security-group-ids ${SGID} | jq .'Instances.PrivateIpAddress' | sed -e 's/"//g')
+                      --security-group-ids ${SGID} | jq '.Instances.PrivateIpAddress' | sed -e 's/"//g')
 
