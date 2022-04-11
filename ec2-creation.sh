36ef6921308b77a6f387734fc31ffa18964069c9
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 15:35:08 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
 fi
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
 
 
 PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
@@ -14,6 +14,22 @@ PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
                       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
                       --security-group-ids ${SGID} \
                       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+                      
+if [ -z "${PRIVATE_IP}" ]; then
+    SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+    if [ -z "${SGID}" ]; then
+        echo -e "\e[31m Security Group doesn't exist\e[0m"
+    fi
+    aws ec2 run-instances --image-id="${AMI_ID}" \
+                          --instance-type=t2.micro \
+                          --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+                          --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+                          --security-group-ids ${SGID} \
+                          | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
+    echo -e "\e[1m Instance Created\e[0m"
+    else
+        echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
+fi                      
 
 ## Creating DNS records
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" \
