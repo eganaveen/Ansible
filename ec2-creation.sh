81a80d197afff90710bb003a59ec3911cc7e89e9
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 15:16:55 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -11,8 +11,8 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all 
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" \
                                         --output text | grep roboshop.internal \
                                         | awk '{print $1}' | awk -F / '{print $3}')
-sed -e 's/IPADDRESS/PRIVATE_IP/' -e '/s/COMPONENT/COMPONENT' roboshop.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "/s/COMPONENT/${COMPONENT}/" roboshop.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id "${ZONE_ID}" --change-batch file:///tmp/record.json | jq
 
 PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
                       --instance-type=t2.micro \
