117b2c62cee4f0c44fa99b435d78624e2f587181
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 20:09:05 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -11,13 +11,6 @@ INSTANE_CREATE(){
 
   PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
-  #PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
-  #                      --instance-type=t2.micro \
-  #                      --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-  #                      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-  #                      --security-group-ids ${SGID} \
-  #                      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-
   if [ -z "${PRIVATE_IP}" ]; then
       SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
       if [ -z "${SGID}" ]; then
@@ -29,10 +22,7 @@ INSTANE_CREATE(){
                             --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
                             --security-group-ids ${SGID} \
                             | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-      echo -e "\e[1m ${COMPONENT} Instance Created\e[0m"
-
-
-#      PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+      echo -e " \e[31m${COMPONENT}\e[0m \e[32mInstance Created\e[0m"
 
       # Creating DNS records
       ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" \
