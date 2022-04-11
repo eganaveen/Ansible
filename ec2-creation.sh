98cc9eb03f25a00b8a98d89a45ed110068af0c1a
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 13:11:29 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -6,5 +6,5 @@ fi
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
-aws ec2 run-instances --image-id="${AMI_ID}" --instance-type=t2.micro --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+aws ec2 run-instances --image-id="${AMI_ID}" --instance-type=t2.micro --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
