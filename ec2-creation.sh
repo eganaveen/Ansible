5556e2b4e9d445dae09c2ed91a22d19c2fa589ed
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 15:12:26 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -6,8 +6,14 @@ fi
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+## Creating DNS records
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" \
-                                        --output text | grep roboshop.internal | awk '{print $1}' | awk -F / '{print $3}')
+                                        --output text | grep roboshop.internal \
+                                        | awk '{print $1}' | awk -F / '{print $3}')
+sed -e 's/IPADDRESS/PRIVATE_IP/' -e '/s/COMPONENT/COMPONENT' roboshop.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file://record.json
+
 PRIVATE_IP=$(aws ec2 run-instances --image-id="${AMI_ID}" \
                       --instance-type=t2.micro \
                       --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
