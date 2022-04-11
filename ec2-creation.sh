6c7bc180f0b434d8f6c2035f3421c3ea37e4968e
Ega Naveen Reddy <enreddy777@gmail.com>
2022-04-11 14:49:23 +0530

    ec2-cration file is added.

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -5,9 +5,12 @@ if [ -z "$1" ]; then
 fi
 COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg_all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" \
+                                        --output text | grep roboshop.internal | awk '{print $1}' | awk -F / '{print $3}')
 aws ec2 run-instances --image-id="${AMI_ID}" \
                       --instance-type=t2.micro \
                       --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-                      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | jq
+                      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+                      --security-group-ids ${SGID}| jq
 
